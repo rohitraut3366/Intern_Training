5630471223fc65a6cbf8c776e42578df15e83b88
rohitraut3366 <59472818+rohitraut3366@users.noreply.github.com>
2021-07-15 11:57:42 +0530

    Update communicate_openstack.py

--- python porgam to connect with OSP/communicate_openstack.py
+++ python porgam to connect with OSP/communicate_openstack.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import sys
 
 import requests
@@ -15,7 +17,7 @@ def api_communicator(end_point, method, api, token):
 param = sys.argv
 if len(param) < 5:
     print("""
-    required: end_point, method[GET | DELETE], api and token
+    Required param: end_point, method[GET | DELETE], api and token
     Syntax: python3 communicate_openstack.py <endpoint> <method> <API> <token>
     Example: python communicate_openstack.py  http://10.0.78.69/image GET /v2/images MGoSF3Ce7M8wsDnUrm6nPG
     """)
